7af32f102df420be4681204248487677b23cb100
Luluka43 <setentrionalhs@gmail.com>
2024-11-13 11:48:11 -0300

    Questao01.py

--- Questao01.py
+++ Questao01.py
@@ -5,3 +5,6 @@ st.write('Sou servidora pública!')
 st.title('Este é o Título do app')
 st.header('Este é o Subtítulo')
 st.subheader('Este é o terceiro subtítulo')
+
+resposta = st.slider('Informe o grau de satisfação:', min_value=0, max_value=100)
+st.write(resposta)
