6f610c3659ea2cf7fb8a20cccee44266cc1236fb
Luluka43 <setentrionalhs@gmail.com>
2024-11-13 11:29:47 -0300

    Update Questao01.py

--- Questao01.py
+++ Questao01.py
@@ -1,3 +1,7 @@
 import streamlit as st
 #saída mensagem exibida na tela
 st.write('Sou servidora pública!')
+
+st.title('Este é o Título do app')
+st.header('Este é o Subtítulo')
+st.subheader('Este é o terceiro subtítulo')
